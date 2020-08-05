457396edd840b694572c723155ad1f7b112be864
Freddy <oldfredddy@gmail.com>
2020-08-05 13:55:38 +0300

    Уведомления

--- app/src/main/java/com/oldfredddy/joke/DelayedMessageService.java
+++ app/src/main/java/com/oldfredddy/joke/DelayedMessageService.java
@@ -1,13 +1,18 @@
 package com.oldfredddy.joke;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.util.Log;
+
+import androidx.core.app.NotificationCompat;
 
 
 public class DelayedMessageService extends IntentService {
 
-    public  static final  String EXTRA_MESSAGE = "message";
+    public static final String EXTRA_MESSAGE = "message";
+    public static final int NOTIFICATION_ID = 5453;
 
 
     public DelayedMessageService() {
@@ -16,22 +21,43 @@ public class DelayedMessageService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-            synchronized (this){
-                try {
-                    wait(10000);
-                } catch (InterruptedException e){
-                    e.printStackTrace();
-                }
+        synchronized (this) {
+            try {
+                wait(3000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
-            //String text = intent.getStringExtra(EXTRA_MESSAGE);
-            String text = (String) intent.getExtras().get(EXTRA_MESSAGE);
-            showText(text);
+        }
+        //String text = intent.getStringExtra(EXTRA_MESSAGE);
+        String text = (String) intent.getExtras().get(EXTRA_MESSAGE);
+        showText(text);
     }
 
-    private void showText(final  String text){
-        Log.v("DelayedMessageService", "The message is: " + text);
-    }
+    private void showText(final String text) {
+        //Log.v("DelayedMessageService", "The message is: " + text);
 
+        //Создание построителя уведомлений
+        NotificationCompat.Builder builder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(android.R.drawable.sym_def_app_icon)
+                        .setContentTitle(getString(R.string.question))
+                        .setContentText(text)
+                        .setPriority(NotificationCompat.PRIORITY_HIGH)
+                        .setVibrate(new long[]{0, 1000})
+                        .setAutoCancel(true);
+
+        //создание действия
+        Intent actionIntent = new Intent(this, MainActivity.class);
+        PendingIntent actionPendingIntent = PendingIntent.getActivity(this,
+                0,
+                actionIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT);
+        builder.setContentIntent(actionPendingIntent);
+
+        //Выдача уведомления
+        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        notificationManager.notify(NOTIFICATION_ID, builder.build());
+    }
 
 
 }
